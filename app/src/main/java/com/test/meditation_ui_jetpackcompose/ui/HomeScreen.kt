70cd27d54dc0e15a5d288247e34d06ed5d557e66
Sagar gandhi <gandhisagar25@gmail.com>
2021-10-25 14:50:49 +0100

    updated GreetingSection

--- app/src/main/java/com/test/meditation_ui_jetpackcompose/ui/HomeScreen.kt
+++ app/src/main/java/com/test/meditation_ui_jetpackcompose/ui/HomeScreen.kt
@@ -76,13 +76,15 @@ fun HomeScreen() {
                 )
             )
         }
-        BottomMenu(items = listOf(
-            BottomMenuContent("Home",R.drawable.ic_home),
-            BottomMenuContent("Meditate",R.drawable.ic_bubble),
-            BottomMenuContent("Sleep",R.drawable.ic_moon),
-            BottomMenuContent("Music",R.drawable.ic_music),
-            BottomMenuContent("Profile",R.drawable.ic_profile),
-        ), modifier = Modifier.align(Alignment.BottomCenter))
+        BottomMenu(
+            items = listOf(
+                BottomMenuContent("Home", R.drawable.ic_home),
+                BottomMenuContent("Meditate", R.drawable.ic_bubble),
+                BottomMenuContent("Sleep", R.drawable.ic_moon),
+                BottomMenuContent("Music", R.drawable.ic_music),
+                BottomMenuContent("Profile", R.drawable.ic_profile),
+            ), modifier = Modifier.align(Alignment.BottomCenter)
+        )
     }
 }
 
@@ -90,9 +92,9 @@ fun HomeScreen() {
 fun BottomMenu(
     items: List<BottomMenuContent>,
     modifier: Modifier = Modifier,
-    activeHightlightColor: Color = ButtonBlue,
+    activeHighlightColor: Color = ButtonBlue,
     activeTextColor: Color = Color.White,
-    inactiveTextolor: Color = AquaBlue,
+    inactiveTextColor: Color = AquaBlue,
     initialSelectedItemIndex: Int = 0
 ) {
     var selectedItemIndex by remember {
@@ -109,10 +111,10 @@ fun BottomMenu(
         items.forEachIndexed { index, item ->
             BottomMenuItem(
                 item = item,
-            isSelected = index == selectedItemIndex,
-            activeHightlightColor = activeHightlightColor,
-            activeTextColor = activeTextColor,
-            inactiveTextolor = inactiveTextolor
+                isSelected = index == selectedItemIndex,
+                activeHighlightColor = activeHighlightColor,
+                activeTextColor = activeTextColor,
+                inactiveTextolor = inactiveTextColor
             ) {
                 selectedItemIndex = index
             }
@@ -124,7 +126,7 @@ fun BottomMenu(
 fun BottomMenuItem(
     item: BottomMenuContent,
     isSelected: Boolean = false,
-    activeHightlightColor: Color = ButtonBlue,
+    activeHighlightColor: Color = ButtonBlue,
     activeTextColor: Color = Color.White,
     inactiveTextolor: Color = AquaBlue,
     onItemClick: () -> Unit
@@ -140,19 +142,19 @@ fun BottomMenuItem(
             contentAlignment = Alignment.Center,
             modifier = Modifier
                 .clip(RoundedCornerShape(10.dp))
-                .background(if (isSelected) activeHightlightColor else Color.Transparent)
+                .background(if (isSelected) activeHighlightColor else Color.Transparent)
                 .padding(10.dp)
-        ){
+        ) {
             Icon(
                 painter = painterResource(id = item.iconId),
                 contentDescription = item.title,
-                tint = if(isSelected) activeTextColor else inactiveTextolor,
+                tint = if (isSelected) activeTextColor else inactiveTextolor,
                 modifier = Modifier.size(20.dp)
             )
         }
         Text(
             text = item.title,
-            color = if(isSelected) activeTextColor else inactiveTextolor
+            color = if (isSelected) activeTextColor else inactiveTextolor
         )
     }
 }
@@ -165,7 +167,7 @@ fun GreetingSection(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
             .padding(15.dp)
     ) {
         Column(
@@ -284,101 +286,6 @@ fun FeatureSection(features: List<Feature>) {
     }
 }
 
-//@Composable
-//fun FeatureItem(
-//    feature: Feature
-//) {
-//    BoxWithConstraints(
-//        modifier = Modifier
-//            .padding(7.5.dp)
-//            .aspectRatio(1f)
-//            .clip(RoundedCornerShape(10.dp))
-//            .background(feature.darkColor)
-//    ) {
-//        val width = constraints.maxWidth
-//        val height = constraints.maxHeight
-//
-//        //Medium Colored path
-//        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
-//        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
-//        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
-//        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
-//        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
-//
-//        val mediumColoredPath = Path().apply {
-//            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
-//            standardQuadFromTo(mediumColoredPoint1,mediumColoredPoint2)
-//            standardQuadFromTo(mediumColoredPoint2,mediumColoredPoint3)
-//            standardQuadFromTo(mediumColoredPoint3,mediumColoredPoint4)
-//            standardQuadFromTo(mediumColoredPoint4,mediumColoredPoint5)
-//            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
-//            lineTo(-100f, height.toFloat() + 100f)
-//            close()
-//        }
-//
-//        // Light colored path
-//        val lightPoint1 = Offset(0f, height * 0.35f)
-//        val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
-//        val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
-//        val lightPoint4 = Offset(width * 0.65f, height.toFloat())
-//        val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 3f)
-//
-//        val lightColoredPath = Path().apply {
-//            moveTo(lightPoint1.x, lightPoint1.y)
-//            standardQuadFromTo(lightPoint1, lightPoint2)
-//            standardQuadFromTo(lightPoint2, lightPoint3)
-//            standardQuadFromTo(lightPoint3, lightPoint4)
-//            standardQuadFromTo(lightPoint4, lightPoint5)
-//            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
-//            lineTo(-100f, height.toFloat() + 100f)
-//            close()
-//        }
-//        Canvas(
-//            modifier = Modifier
-//                .fillMaxSize()
-//        ) {
-//            drawPath(
-//                path = mediumColoredPath,
-//                color = feature.mediumColor
-//            )
-//            drawPath(
-//                path = lightColoredPath,
-//                color = feature.lightColor
-//            )
-//        }
-//        Box(modifier = Modifier
-//            .fillMaxSize()
-//            .padding(15.dp)
-//        ) {
-//            Text(
-//                text = feature.title,
-//                style = MaterialTheme.typography.h2,
-//                lineHeight = 26.sp,
-//                modifier = Modifier.align(Alignment.TopStart)
-//            )
-//            Icon(
-//                painter = painterResource(id = feature.iconId),
-//                contentDescription = feature.title,
-//                tint = Color.White,
-//                modifier = Modifier.align(Alignment.BottomStart)
-//            )
-//            Text(
-//                text = "Start",
-//                color = TextWhite,
-//                fontSize = 14.sp,
-//                fontWeight = FontWeight.Bold,
-//                modifier = Modifier
-//                    .clickable {
-//
-//                    }
-//                    .align(Alignment.BottomEnd)
-//                    .clip(RoundedCornerShape(10.dp))
-//                    .background(ButtonBlue)
-//                    .padding(vertical = 6.dp, horizontal = 15.dp)
-//            )
-//        }
-//    }
-//}
 @Composable
 fun FeatureItem(
     feature: Feature
@@ -393,7 +300,7 @@ fun FeatureItem(
         val width = constraints.maxWidth
         val height = constraints.maxHeight
 
-        // Medium colored path
+        //Medium Colored path
         val mediumColoredPoint1 = Offset(0f, height * 0.3f)
         val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
         val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
@@ -465,7 +372,7 @@ fun FeatureItem(
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier
                     .clickable {
-                        // Handle the click
+
                     }
                     .align(Alignment.BottomEnd)
                     .clip(RoundedCornerShape(10.dp))
